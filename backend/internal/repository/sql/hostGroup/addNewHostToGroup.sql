1b0ce36e4d211b8d6b895bd78ce51b1a8368f4a1
nihar <niharmeena4114@gmail.com>
2025-07-11 00:18:48 +0530

    fix duplicate host in hostgroup

--- backend/internal/repository/sql/hostGroup/addNewHostToGroup.sql
+++ backend/internal/repository/sql/hostGroup/addNewHostToGroup.sql
@@ -1,4 +1,6 @@
 UPDATE host_group
-SET host_ids = host_ids || $2,
-    updated_at = NOW()
+SET host_ids = (
+    SELECT ARRAY(SELECT DISTINCT val FROM unnest(host_ids || $2) AS val)
+),
+updated_at = NOW()
 WHERE id = $1;
