001e1d9e9986b2b7827a72070c042265e195d58a
nihar <niharmeena4114@gmail.com>
2025-07-14 14:35:20 +0530

    fix deployment changes

--- backend/internal/model/deployment/deployment.go
+++ backend/internal/model/deployment/deployment.go
@@ -5,21 +5,20 @@ import "time"
 type Deployment struct {
 	ID          string           `db:"id" json:"id"`
 	UserID      string           `db:"user_id" json:"userId"`
-	HostID      int          `db:"host_id" json:"hostId"`
-	HostGroupID int           `db:"host_group_id" json:"hostGroupId"`
-	BlueprintID int           `db:"blueprint_id" json:"blueprintId"`
+	HostID      int              `db:"host_id" json:"hostId"`
+	HostGroupID int              `db:"host_group_id" json:"hostGroupId"`
+	BlueprintID int              `db:"blueprint_id" json:"blueprintId"`
 	Type        DeploymentType   `db:"type" json:"type"`     // "plan" or "deploy"
 	Status      DeploymentStatus `db:"status" json:"status"` // "pending", "started", etc.
 	CreatedAt   time.Time        `db:"created_at" json:"createdAt"`
 	UpdatedAt   time.Time        `db:"updated_at" json:"updatedAt"`
 }
 
-
 type DeploymentMessage struct {
-	JobID       string   `json:"jobId"`
-	UserID      string   `json:"userId"`
-	HostIDs     []int `json:"hostIds"`
-	BlueprintID string   `json:"blueprintId"`
-	Type        string   `json:"type"`       // always "deploy"
-	CreatedAt   string   `json:"created_at"` // ISO8601 string
+	JobID       string         `json:"jobId"`
+	UserID      string         `json:"userId"`
+	HostIDs     []int          `json:"hostIds"`
+	BlueprintID int            `json:"blueprintId"`
+	Type        DeploymentType `json:"type"`
+	CreatedAt   time.Time      `json:"created_at"`
 }
--- backend/internal/repository/sql/deployment/createDeployment.sql
+++ backend/internal/repository/sql/deployment/createDeployment.sql
@@ -4,4 +4,5 @@ INSERT INTO deployments (
 VALUES (
   :id, :user_id, :host_id, :host_group_id, :blueprint_id, :type, :status
 )
-ON CONFLICT (id) DO NOTHING;
+ON CONFLICT (id) DO NOTHING
+RETURNING *;
