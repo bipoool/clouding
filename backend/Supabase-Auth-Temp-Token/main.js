402b419817f652b3b31792c50788063ce6425fe0
nihar <niharmeena4114@gmail.com>
2025-07-14 14:37:56 +0530

    remove supabase key

--- backend/Supabase-Auth-Temp-Token/main.js
+++ backend/Supabase-Auth-Temp-Token/main.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // load from your env
 const supabaseUrl = "https://ikzofffjwkqpiwrqsttb.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlrem9mZmZqd2txcGl3cnFzdHRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3NDkyNzAsImV4cCI6MjA2NzMyNTI3MH0.dHR1DlUQQ4W4pQHkxsw5MmI2f1QOgcCRnZ4UkC0DHyE";
+const supabaseKey = "";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
