c90cb7207181bfafaf3db2f21d867d0d1d16cd5a
Vipul Gupta <86769501+bipoool@users.noreply.github.com>
2025-08-04 19:57:59 +0530

    Deployement worker (#37)

--- ansible-server/deployment-worker/ansible-runner.py
+++ ansible-server/deployment-worker/ansible-runner.py
@@ -5,37 +5,17 @@ import os
 from ansible_runner.runner import Runner
 from fastapi.middleware.cors import CORSMiddleware
 
-# app = FastAPI()
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],  # or ["http://localhost:5500"] for stricter
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-# @app.get("/run")
-# def run_playbook(playbook: str = Query(..., description="Path to the Ansible playbook")):
-#     """
-#     SSE endpoint to run the playbook and stream its progress and logs.
-#     Accepts a 'playbook' query parameter for the playbook path.
-#     """
-
-playbook = "myplaybook.yml"
-# Validate playbook path
-if not os.path.isfile(playbook):
-    raise HTTPException(status_code=400, detail=f"Playbook not found: {playbook}")
-
-def event_generator():
+def ansibleRunner(playbookPath: str):
     try:
         thread, runner = ansible_runner.run_async(
             private_data_dir=".",
-            playbook=playbook,
+            playbook=playbookPath,
             cmdline='--check --diff',
             quiet=True
         )
+
     except Exception as e:
-        # yield {"event": "error", "data": f"Failed to start runner: {str(e)}"}
-        return
+        raise HTTPException(status_code=400, detail=f"Failed to start runner: {str(e)}")
 
     while thread.is_alive():
         processed_events = set()
@@ -74,6 +54,3 @@ def event_generator():
     #     "data": "Playbook finished"
     # }
     print("event end: ", "Playbook finished")
-
-event_generator()
-# return EventSourceResponse(event_generator())
