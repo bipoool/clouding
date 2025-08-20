a96fb984ea7066bcadd5b555d796738ca16484b7
bipoool <itsmevipulgupta.2011@gmail.com>
2025-08-21 00:42:16 +0530

    Rebased with main

--- backend/internal/repository/sql/deployment/getDeploymentById.sql
+++ backend/internal/repository/sql/deployment/getDeploymentById.sql
@@ -1,8 +1,4 @@
 SELECT 
-<<<<<<< HEAD
   id, user_id, blueprint_id, type, status, created_at, updated_at
-=======
-  id, user_id, host_id, host_group_id, blueprint_id, type, status, created_at
->>>>>>> 54be485 (add deployment changes)
 FROM deployments
 WHERE id = $1;
--- backend/internal/repository/sql/deployment/getDeploymentByUser.sql
+++ backend/internal/repository/sql/deployment/getDeploymentByUser.sql
@@ -1,14 +1,6 @@
 SELECT 
-<<<<<<< HEAD
   id, user_id, blueprint_id, type, status, created_at
-=======
-  id, user_id, host_id, host_group_id, blueprint_id, type, status, created_at
->>>>>>> 54be485 (add deployment changes)
 FROM deployments
 WHERE user_id = $1
   AND type = $2
 ORDER BY created_at DESC;
-<<<<<<< HEAD
-=======
-
->>>>>>> 54be485 (add deployment changes)
